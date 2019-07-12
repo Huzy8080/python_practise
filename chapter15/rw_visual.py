054780d685e9442039cd988beedb27da4bebdc47
huzhaoyang <zhaoyang_hu@kingdee.com>
2019-07-12 15:45:55 +0800

    数据可视化

--- chapter15/rw_visual.py
+++ chapter15/rw_visual.py
@@ -6,7 +6,12 @@ while True:
 
     rw = random_walk.RandomWalk()
     rw.fill_walk()
-    plt.scatter(rw.x_values, rw.y_values, s=15)
+    plt.scatter(rw.x_values, rw.y_values, s=5, c=rw.y_values, cmap=plt.cm.Blues)
+
+    # 突出起点和终点
+    plt.scatter(0, 0, c='green', edgecolors='none', s=50)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='green', edgecolors='none', s=50)
+
     plt.show()
     keep_running = input("Make another walk? (y/n): ")
     if keep_running == 'n':
